b532cf400bdbd417e048246d76d850ca5bda62ee
Michał Bartkowiak <bartmichu@gmail.com>
2019-06-04 14:05:28 +0200

    Add new script

---
+++ find_dodgy_files.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "Starting test 1/5 ..."
+/usr/bin/find / -perm -2000 -ls > find_2000-ls.txt &&
+echo "... 1/5 finished"
+
+echo "Starting test 2/5 ..."
+/usr/bin/find / -perm -4000 -ls > find_4000-ls.txt &&
+echo "... 2/5 finished"
+
+echo "Starting test 3/5 ..."
+/usr/bin/find / -perm -o+w -type f -ls > find_ow_f-ls.txt &&
+echo "... 3/5 finished"
+
+echo "Starting test 4/5 ..."
+/usr/bin/find / -perm -o+w -type d -ls > find_ow_d-ls.txt &&
+echo "... 4/5 finished"
+
+echo "Starting test 5/5 ..."
+/usr/bin/find / -nouser -o -nogroup -print > find_no.txt &&
+echo "... 5/5 finished"
