5b59c44881e5147927f6106166d2004956cf7561
Michał Bartkowiak <bartmichu@gmail.com>
2019-11-06 21:41:18 +0100

    Rename variable

--- need_reboot.sh
+++ need_reboot.sh
@@ -4,9 +4,9 @@
 # This script works with unmodified GENERIC kernels distributed as a part
 # of an official FreeBSD release.
 
-email_to="root"
+email_address="root"
 email_subject="`/bin/hostname` - Reboot Required"
 
 if [ `/bin/freebsd-version` != `/usr/bin/uname -r` ]; then
-  echo "A reboot is required following updates to this server: `/bin/hostname`" | /usr/bin/mail -s "$email_subject" $email_to
+  echo "A reboot is required following updates to this server: `/bin/hostname`" | /usr/bin/mail -s "$email_subject" $email_address
 fi
