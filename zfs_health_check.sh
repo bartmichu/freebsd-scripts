02c333799d8fd9bae1241bd309640bf20031b4d0
Michał Bartkowiak <bartmichu@gmail.com>
2019-11-06 21:42:39 +0100

    Change variable naming convention

--- zfs_health_check.sh
+++ zfs_health_check.sh
@@ -4,15 +4,15 @@
 # Based on https://calomel.org/zfs_health_check_script.html by Calomel.org
 #
 
-capacityThreshold=80
-scrubThreshold=35
-emailTo="root"
+capacity_threshold=80
+scrub_threshold=35
+email_to="root"
 
 condition=$(/sbin/zpool status | egrep -i '(DEGRADED|FAULTED|OFFLINE|UNAVAIL|REMOVED|FAIL|DESTROYED|corrupt|cannot|unrecover)')
 problems=0
 
 if [ "${condition}" ]; then
-        emailSubject="`hostname` - ZFS pool - HEALTH fault"
+        email_subject="`hostname` - ZFS pool - HEALTH fault"
         problems=1
 fi
 
@@ -20,8 +20,8 @@ if [ ${problems} -eq 0 ]; then
    capacity=$(/sbin/zpool list -H -o capacity | cut -d'%' -f1)
    for line in ${capacity}
      do
-       if [ $line -ge $capacityThreshold ]; then
-         emailSubject="`hostname` - ZFS pool - Capacity Exceeded"
+       if [ $line -ge $capacity_threshold ]; then
+         email_subject="`hostname` - ZFS pool - Capacity Exceeded"
          problems=1
        fi
      done
@@ -30,16 +30,16 @@ fi
 if [ ${problems} -eq 0 ]; then
    errors=$(/sbin/zpool status | grep ONLINE | grep -v state | awk '{print $3 $4 $5}' | grep -v 000)
    if [ "${errors}" ]; then
-        emailSubject="`hostname` - ZFS pool - Drive Errors"
+        email_subject="`hostname` - ZFS pool - Drive Errors"
         problems=1
    fi
 fi
 
 if [ ${problems} -eq 0 ]; then
-   currentDate=$(date +%s)
-   zfsVolumes=$(/sbin/zpool list -H -o name)
+   current_date=$(date +%s)
+   zfs_volumes=$(/sbin/zpool list -H -o name)
 
-  for volume in ${zfsVolumes}
+  for volume in ${zfs_volumes}
    do
     if [ $(/sbin/zpool status $volume | egrep -c "none requested") -ge 1 ]; then
         printf "ERROR: You need to run \"zpool scrub $volume\" before this script can monitor the scrub expiration time."
@@ -57,14 +57,14 @@ if [ ${problems} -eq 0 ]; then
     
     scrubDate=$(date -j -f '%Y%b%e-%H%M%S' $scrubRawDate'-000000' +%s)
 
-     if [ $(($currentDate - $scrubDate)) -ge $(($scrubThreshold * 24 * 60 * 60)) ]; then
-        emailSubject="`hostname` - ZFS pool - Scrub Time Expired. Scrub Needed on Volume(s)"
+     if [ $(($current_date - $scrubDate)) -ge $(($scrub_threshold * 24 * 60 * 60)) ]; then
+        email_subject="`hostname` - ZFS pool - Scrub Time Expired. Scrub Needed on Volume(s)"
         problems=1
      fi
    done
 fi
 
 if [ "$problems" -ne 0 ]; then
-  printf '%s\n' "$emailSubject" "" "`/sbin/zpool list`" "" "`/sbin/zpool status`" | /usr/bin/mail -s "$emailSubject" $emailTo
-  logger $emailSubject
+  printf '%s\n' "$email_subject" "" "`/sbin/zpool list`" "" "`/sbin/zpool status`" | /usr/bin/mail -s "$email_subject" $email_to
+  logger $email_subject
 fi
